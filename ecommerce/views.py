3274aa8cf4cedefe61a60bd9664840641969b8d4
Tyler J Thomas <tye.thomas17@gmail.com>
2017-04-14 08:01:55 -0500

    Show products on Homepage

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
-from products.models import ProductFeatured
+from products.models import ProductFeatured, Product
 from products.models import Category
 
 def home(request):
 	featured_image = ProductFeatured.objects.first()
 	categories_list = Category.objects.all()
+	products = Product.objects.all()
 
 	context = {
 		"featured_image": featured_image,
-		"categories_list": categories_list
+		"categories_list": categories_list,
+		"products": products,
 	}
 	return render(request, "home.html", context)
