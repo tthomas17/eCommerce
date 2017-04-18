f1be25ba8cb137b99f8ab1f6b70b60a8b158bb42
Tyler J Thomas <tye.thomas17@gmail.com>
2017-04-18 09:04:15 -0500

    Added Cart App and implemented AJAX for add/update cart

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
+
+from carts.views import CartView
 from . import views
 
 urlpatterns = [
@@ -10,6 +12,8 @@ urlpatterns = [
     url (r'^accounts/', include('registration.backends.default.urls')),
     url (r'^products/', include('products.urls')),
     url (r'^categories/', include('products.urls_categories')),
+    url (r'^cart/$', CartView.as_view(), name='cart'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ static/js/custom.js
@@ -0,0 +1,12 @@
+function showFlashMessage(message){
+		var template = "<div class='container flash-alert-box'>" +
+		"<div class='col-sm-3 col-sm-offset-8'>" +
+		"<div class='alert alert-success alert-dismissible' role='alert'>" +
+		"<button type='button' class='close' data-dismiss='alert' aria-label='Close'>"+
+		"<span aria-hidden='true'>&times;</span></button>" + message + "</div></div></div>"
+		$("body").append(template);
+		$(".flash-alert-box").fadeIn();
+		setTimeout(function(){
+			$(".flash-alert-box").fadeOut();
+		}, 1800);
+	}
